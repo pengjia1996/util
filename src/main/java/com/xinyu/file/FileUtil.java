f7551bec92961cbbee771f4d82469c7da23f3f7b
pengjia <2776248656>
2021-05-09 19:01:34 +0800

    格式优化

--- src/main/java/com/xinyu/file/FileUtil.java
+++ src/main/java/com/xinyu/file/FileUtil.java
@@ -1,33 +1,28 @@
 package com.xinyu.file;
 
-import java.io.File;
-
-import org.apache.log4j.Logger;
-
 import com.xinyu.util.QwyUtil;
 
-/**   
- * @Title:FileUtil
- * @author:彭嘉
- * @date:2020年12月3日 下午3:01:03  
- * @Description:文件公共类   
+import java.io.File;
+
+
+/**
+ * 文件公共类
+ *
+ * @author pengjia
  */
 public class FileUtil {
 
-    private static Logger logger = Logger.getLogger(FileUtil.class);
 
     /**
-     * 
-     * @Title:exist
-     * @author:彭嘉
-     * @date:2020年12月9日 上午11:19:55  
-     * @Description: 根据路径判断文件是否存在  
+     * 根据路径判断文件是否存在
      * @param path 文件路径
      * @return boolean
+     * @author pengjia
+     * @date 2021/5/9 17:49
      */
     public static boolean exist(String path) {
         if (QwyUtil.isNullAndEmpty(path)) {
-            logger.info("path is null,文件路径不能为空");
+            System.err.println("path is null,文件路径不能为空");
             return false;
         }
         return new File(path).exists();
