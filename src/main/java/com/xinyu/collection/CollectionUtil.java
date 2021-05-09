80d07bc138bdf9c5c3c8acfd58ecd4b72e7689a7
pengjia <2776248656>
2021-05-09 17:08:37 +0800

    集合公共类格式优化

--- src/main/java/com/xinyu/collection/CollectionUtil.java
+++ src/main/java/com/xinyu/collection/CollectionUtil.java
@@ -12,25 +12,24 @@ import com.xinyu.reflect.ReflectUtil;
 import com.xinyu.util.QwyUtil;
 
 
-
-/**   
+/**
  * @Title:CollectionUtil
  * @author:彭嘉
- * @date:2020年10月26日 下午5:28:35  
- * @Description:集合操作公共类   
+ * @date:2020年10月26日 下午5:28:35
+ * @Description:集合操作公共类
  */
 @SuppressWarnings("all")
 public class CollectionUtil {
-    private static Logger logger = Logger.getLogger(CollectionUtil.class);
+
 
     /**
-     * @Title:listToMap
-     * @author:彭嘉
-     * @date:2019年9月30日 上午11:08
-     * @Description: 将list转成map，对象中指定属性（kName）的值作为map的key,对象作为map的value
+     * 将list转成map，对象中指定属性（kName）的值作为map的key,对象作为map的value
+     *
      * @param vList 需要转换成map的list
      * @param kName list中对象的属性名称,(用于将该属性的值作为map的key,一般为对象的id)
-     * @return Map
+     * @return java.util.Map<K, V>
+     * @author pengjia
+     * @date 2021/5/9 16:51
      */
     public static <K, V> Map<K, V> listToMap(List<V> vList, String kName) {
         // 需要返回的map
@@ -38,7 +37,7 @@ public class CollectionUtil {
         try {
             // 判断集合是否为空，属性名是否为空，为空不能转化，返回null
             if (QwyUtil.isNullAndEmpty(vList) || QwyUtil.isNullAndEmpty(kName)) {
-                logger.info("vList or kName is null");
+                System.err.println("vList or kName is null");
                 return map;
             }
             // 获取kName的Field
@@ -46,28 +45,26 @@ public class CollectionUtil {
             if (!QwyUtil.isNullAndEmpty(kField)) {
                 // 循环lsit,获取对象指定kName的属性的值，作为key
                 for (V v : vList) {
-                    map.put((K)kField.get(v), v);
+                    map.put((K) kField.get(v), v);
                 }
             } else {
-                logger.info("根据kName，获取不到Field(对象的属性)");
+                System.err.println("根据kName，获取不到Field(对象的属性) kName=" + kName);
             }
         } catch (Exception e) {
-            logger.error("将list转成map err", e);
+            e.printStackTrace();
         }
         return map;
     }
 
+
     /**
-     * 
-     * @Title:listToMapList
-     * @author:彭嘉
-     * @date:2020年10月27日 上午9:07:37  
-     * @Description: List 转Map<K,List<V>>   
-     * @param <K> key的类型
-     * @param <V> 集合中对象的类型
+     * List 转Map<K,List<V>>
+     *
      * @param vList 要转换的集合
      * @param kName list中对象的属性名称,(用于将该属性的值作为map的key)
-     * @return  Map<K,List<V>>
+     * @return java.util.Map<K, java.util.List < V>>
+     * @author pengjia
+     * @date 2021/5/9 16:55
      */
     public static <K, V> Map<K, List<V>> listToMapList(List<V> vList, String kName) {
         // 需要返回的map
@@ -75,7 +72,7 @@ public class CollectionUtil {
         try {
             // 判断集合是否为空，属性名是否为空，为空不能转化，返回
             if (QwyUtil.isNullAndEmpty(vList) || QwyUtil.isNullAndEmpty(kName)) {
-                logger.info("vList or kName is null");
+                System.err.println("vList or kName is null");
                 return map;
             } else {
                 // 获取kName的Field
@@ -84,7 +81,7 @@ public class CollectionUtil {
                 if (!QwyUtil.isNullAndEmpty(kField)) {
                     // 循环lsit,获取对象指定kName的属性的值，作为key
                     for (V v : vList) {
-                        K key = (K)kField.get(v);
+                        K key = (K) kField.get(v);
                         if (map.containsKey(key)) {
                             list = map.get(key);
                         } else {
@@ -95,85 +92,93 @@ public class CollectionUtil {
 
                     }
                 } else {
-                    logger.info("根据kName，获取不到Field(对象的属性)");
+                    System.err.println("根据kName，获取不到Field(对象的属性) kName=" + kName);
                 }
                 return map;
             }
         } catch (Exception e) {
-            logger.error("将list转成map<T,List> err", e);
+            e.printStackTrace();
         }
         return map;
     }
 
+
     /**
-     * 
-     * @Title:getKeyByList
-     * @author:彭嘉
-     * @date:2020年9月29日 下午4:17:13  
-     * @Description:   拼接泛型集合中对象的某属性 返回格式 'xxx','xxx','xxx'
-     * @param <T> 集合的类型
+     * 拼接泛型集合中对象的某属性 返回格式 'xxx','xxx','xxx'
+     *
      * @param list 泛型集合
-     * @param key 集合中对象的属性名称
-     * @return String 拼接后的字符串
+     * @param key  集合中对象的属性名称
+     * @return java.lang.String
+     * @author pengjia
+     * @date 2021/5/9 16:57
      */
     public static <T> String getKeyByList(List<T> list, String key) {
         return getKeyByList(list, key, ",", "'");
     }
 
     /**
+     * @param tList
+     * @param kName
+     * @param separator
+     * @return String
      * @Title:toSplitStrByListObjKey
      * @author:彭嘉
      * @date:2019年9月30日 下午13:08
-     * @Description 
-     * @param tList 需要转换成map的list 
-     * @param kName list中对象的属性名称 
+     * @Description
+     */
+
+    /**
+     * 拼接泛型集合中对象的某属性
+     *
+     * @param tList     需要转换成map的list
+     * @param kName     list中对象的属性名称
      * @param separator 拼接的分隔符
-     * @return String
+     * @return java.lang.String
+     * @author pengjia
+     * @date 2021/5/9 16:58
      */
     public static <T> String getKeyByList(List<T> tList, String kName, String separator) {
         return getKeyByList(tList, kName, separator, null);
     }
 
+
     /**
-     * @Title:getKeyByList
-     * @author:彭嘉
-     * @date:2019年9月30日 下午13:08
-     * @Description 
-     * 
      * 将泛型集合list中某个属性的值拼装成string <br>
      * 例如，想将List<User> 中user对象的所有id已 ',' 拼装起来 ，参数kName 为"id",separated 为"," <br>
      * 最终格式为 xxx,xxx,xxx <br>
      * 如果需要的格式为: 'xxx','xxx','xxx' 参数includeStr的值为 ' <br>
      * 注意:kName的值必须是对象已有的属性，不能写错，根据kName获取对象的值必须能转换成字符串
-     * 
-     * @param tList 需要转换成map的list 
-     * @param kName list中对象的属性名称 
-     * @param separator 拼接的分隔符
+     *
+     * @param tList      需要转换成map的list
+     * @param kName      list中对象的属性名称
+     * @param separator  拼接的分隔符
      * @param includeStr 元素被包裹的符号
-     * @return String
+     * @return java.lang.String
+     * @author pengjia
+     * @date 2021/5/9 17:00
      */
     public static <T> String getKeyByList(List<T> tList, String kName, String separator, String includeStr) {
         StringBuffer sb = new StringBuffer();
         try {
             if (QwyUtil.isNullAndEmpty(tList)) {
-                logger.info("vList is null,要拼接的集合不能为空");
+                System.err.println("vList is null,要拼接的集合不能为空");
                 return "";
             }
 
             if (QwyUtil.isNullAndEmpty(kName)) {
-                logger.info("kName is null,集合中元素的属性名称不能为空");
+                System.err.println("kName is null,集合中元素的属性名称不能为空");
                 return "";
             }
 
             if (QwyUtil.isNullAndEmpty(separator)) {
-                logger.info("separator is null,要拼接的分隔符不能为空");
+                System.err.println("separator is null,要拼接的分隔符不能为空");
                 return "";
             }
 
             // 获取kName的Field
             Field kField = ReflectUtil.getField(tList.get(0).getClass(), kName);
             if (QwyUtil.isNullAndEmpty(kField)) {
-                logger.info("根据kName，获取不到Field(对象的属性)");
+                System.err.println("根据kName，获取不到Field(对象的属性)");
                 return "";
             }
 
@@ -181,13 +186,13 @@ public class CollectionUtil {
                 // 循环lsit,获取对象指定kName的属性的值，作为key
                 for (T t : tList) {
                     if (kField.get(t) != null) {
-                        sb.append((String)kField.get(t) + separator);
+                        sb.append((String) kField.get(t) + separator);
                     }
                 }
             } else {
                 for (T t : tList) {
                     if (kField.get(t) != null) {
-                        sb.append(includeStr + (String)kField.get(t) + includeStr + separator);
+                        sb.append(includeStr + (String) kField.get(t) + includeStr + separator);
                     }
                 }
             }
@@ -197,7 +202,8 @@ public class CollectionUtil {
                 return sb.deleteCharAt(sb.length() - 1).toString();
             }
         } catch (Exception e) {
-            logger.error("getKeyByList,将list中某个属性的值拼装成string err", e);
+            System.err.println("将list中某个属性的值拼装成string err");
+
         }
         return "";
     }
